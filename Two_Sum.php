69f9d79493865d34702d75e9e6cc10c0d9571e23
Magomed_Abdullaev <abdulalev.m@05.ru>
2023-03-13 11:47:42 +0300

    Add Two_Sum

---
+++ Two_Sum.php
@@ -0,0 +1,25 @@
+<?php
+
+    function twoSum($nums, $target) {
+        foreach ($nums as $key => $value)
+        {
+                unset($nums[$key]);
+                $value2 = array_search($target-$value, $nums);
+
+                if (gettype($value2)!='boolean')
+                {
+                    $n1=$key;
+                    $n2=$value2;
+                    break;
+                }
+        }
+        return [$n1,$n2];
+    }
+
+//    $test_mass = array(3,3);
+//    $target = 6;
+
+    $answer= twoSum([-3,4,3,90],0);
+    foreach ($answer as $value){
+        echo $value;
+    }
